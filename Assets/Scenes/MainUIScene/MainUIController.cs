a1f4ea6896dc7105cdc4f5544c769db31fa315ee
bldev2473 <bldev2473@gmail.com>
2021-01-21 23:05:13 +0900

    모듈의 초기 체크 인

---
+++ Assets/Scenes/MainUIScene/MainUIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUIController : MonoBehaviour
+{
+	public string loadSceneName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        Destroy(this.gameObject);
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene(loadSceneName, LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject capturedImagePrefab;
+    private Object[] textures;
+    private Sprite sprite;
+    private Texture2D myTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawGrid();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawGrid()
+    {
+        GameObject newObj;
+
+        // Load textures from image files
+        textures = Resources.LoadAll("Images", typeof(Texture2D));
+
+        for (int i = 0; i < textures.Length; i++)
+        {
+            newObj = (GameObject)Instantiate(capturedImagePrefab, transform);
+
+            // Apply EventTrigger to GameObject
+            EventTrigger trigger = newObj.AddComponent<EventTrigger>();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+
+            // Apply images to GameObject
+
+            // Create sprite
+            myTexture = (Texture2D)textures[i];
+            sprite = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f));
+            newObj.GetComponent<Image>().sprite = sprite;
+
+            // Copy object for closure problem
+            GameObject go = newObj;
+
+            // Callback
+            entry.callback.AddListener((eventData) => { ImageClickEvent(go); });
+            trigger.triggers.Add(entry);
+        }
+    }
+
+    void ImageClickEvent(GameObject go)
+    {
+        Debug.Log("Image Clicked" + go.transform.position);
+    }
+}
