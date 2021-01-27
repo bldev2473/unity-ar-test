addd92762fffa13dd13997c5491c206a9b038b6e
bldev2473 <bldev2473@gmail.com>
2021-01-27 23:59:15 +0900

    Initial commit

---
+++ Assets/Scripts/ModelController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ModelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private GameObject player;
+    public string actionName;
+    private float force = 5f;
+    private bool buttonPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player") as GameObject;
+        Debug.Log("Player" + player.ToString());
+    }
+
+    void Update()
+    {
+        if (buttonPressed)
+        {
+            if (actionName.Equals("Forward"))
+            {
+                //player.transform.position = player.transform.position + -1 * player.transform.forward * 1;
+                player.GetComponent<Rigidbody>().velocity = Vector3.forward * force;
+            }
+
+            if (actionName.Equals("Backward"))
+            {
+                //player.transform.position = player.transform.position + -1 * player.transform.forward * 1;
+                player.GetComponent<Rigidbody>().velocity = Vector3.back * force;
+            }
+
+            if (actionName.Equals("Left"))
+            {
+                //player.transform.position = player.transform.position + -1 * player.transform.right * 1;
+                player.GetComponent<Rigidbody>().velocity = Vector3.left * force;
+            }
+
+            if (actionName.Equals("Right"))
+            {
+                //player.transform.position = player.transform.position + player.transform.right * 1;
+                player.GetComponent<Rigidbody>().velocity = Vector3.right * force;
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        player = GameObject.FindGameObjectWithTag("Player") as GameObject;
+        Debug.Log("Player" + player.ToString());
+        buttonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
+}
