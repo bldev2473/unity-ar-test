c6973b6b640ac50a6a45f730466eced4afdb8287
bldev2473 <bldev2473@gmail.com>
2021-02-16 22:52:45 +0900

    Add feature for controlling movement of light source with slider

---
+++ Assets/Scripts/LightSourceMovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LightSourceMovementController : MonoBehaviour
+{
+    public Slider slider;
+    public Light directionalLight;
+
+    private float timeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter = Time.deltaTime;
+
+        float x = Mathf.Cos(timeCounter);
+        float y = Mathf.Sin(timeCounter);
+        float z = 0;
+
+        directionalLight.transform.position = new Vector3(x, y, z);
+    }
+}
