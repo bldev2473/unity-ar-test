3e066c4b0371039173f3180228c8fd8c03f68ea7
bldev2473 <bldev2473@gmail.com>
2021-03-24 18:16:25 +0900

    Create script for flow control

---
+++ Assets/Scripts/ScriptManager/Script1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ScriptManager;
+
+public class Script1 : MonoBehaviour, ScriptManagerInterface
+{
+    System.Object CrossScriptInfo;
+
+    public void OnCompleted()
+    {
+        ScriptManager.OnCompleted += CustomAction;
+    }
+
+    Tuple<MonoBehaviour, System.Object> CustomAction()
+    {
+        this.enabled = false;
+        return Tuple.Create((MonoBehaviour)this, CrossScriptInfo);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptManager/ScriptManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptManager : MonoBehaviour
+{
+    /* 
+     * 1. 메인 화면
+     * 2. (모델 선택 팝업 -> 모델 선택)
+     * 3. (바닥 인식 (가이드 출력) -> 인디케이터 출력 -> 모델 생성 버튼 출력)
+     * 4. 모델 생성
+     * 5. 사람 인식/터치 인식/애니케이션 컨트롤
+     */
+
+    public delegate Tuple<MonoBehaviour, System.Object> ScriptCompletedAction();
+    public static event ScriptCompletedAction OnCompleted;
+
+    private Tuple<MonoBehaviour, System.Object> CurrentScript;
+
+    [SerializeField]
+    private Script1 ModelSelectManager;
+    [SerializeField]
+    private Script2 HumanDetectionManager;
+    [SerializeField]
+    private Script3 ARInteractionManager;
+    [SerializeField]
+    private Script4 AnimationControlManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ModelSelectManager.enabled = true;
+        HumanDetectionManager.enabled = false;
+        ARInteractionManager.enabled = false;
+        AnimationControlManager.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnCompleted != null)
+        {
+            CurrentScript = OnCompleted();
+            ScriptManager.OnCompleted = null;
+        }
+    }
+}
