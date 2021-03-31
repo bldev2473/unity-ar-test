a167021f1de9227b6c00decaf137199ecafda19b
bldev2473 <bldev2473@gmail.com>
2021-03-31 22:47:58 +0900

    Remove URP and add some prefabs / create new scene for ar plane change feature

--- Assets/Scripts/Light&Shadow/ShadowResolutionController.cs
+++ Assets/Scripts/Light&Shadow/ShadowResolutionController.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UI;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Rendering;
+//using UnityEngine.Rendering.Universal;
+//using UnityEngine.UI;
 
-public class ShadowResolutionController : MonoBehaviour
-{
-    public Toggle toggle;
-    private RenderPipelineAsset rpAsset;
-    private UniversalRenderPipelineAsset urpAsset;
-    private SROptions sro;
+//public class ShadowResolutionController : MonoBehaviour
+//{
+//    public Toggle toggle;
+//    private RenderPipelineAsset rpAsset;
+//    private UniversalRenderPipelineAsset urpAsset;
+//    private SROptions sro;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rpAsset = UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset;
-        urpAsset = (UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset)rpAsset;
-        sro = new SROptions();
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        rpAsset = UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset;
+//        urpAsset = (UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset)rpAsset;
+//        sro = new SROptions();
 
-        //Fetch the Toggle GameObject
-        toggle = GetComponent<Toggle>();
+//        //Fetch the Toggle GameObject
+//        toggle = GetComponent<Toggle>();
 
-        //Add listener for when the state of the Toggle changes, to take action
-        toggle.onValueChanged.AddListener(delegate {
-            ToggleValueChanged(toggle);
-        });
-    }
+//        //Add listener for when the state of the Toggle changes, to take action
+//        toggle.onValueChanged.AddListener(delegate {
+//            ToggleValueChanged(toggle);
+//        });
+//    }
 
-    // Update is called once per frame
-    void ToggleValueChanged(Toggle toggle)
-    {
-        if (toggle.isOn)
-        {
-            sro.MainLightShadowResolution = UnityEngine.Rendering.Universal.ShadowResolution._4096;
-        }
-        else
-        {
-            sro.MainLightShadowResolution = UnityEngine.Rendering.Universal.ShadowResolution._512;
-        }
+//    // Update is called once per frame
+//    void ToggleValueChanged(Toggle toggle)
+//    {
+//        if (toggle.isOn)
+//        {
+//            sro.MainLightShadowResolution = UnityEngine.Rendering.Universal.ShadowResolution._4096;
+//        }
+//        else
+//        {
+//            sro.MainLightShadowResolution = UnityEngine.Rendering.Universal.ShadowResolution._512;
+//        }
         
-    }
-}
+//    }
+//}
---
+++ Assets/Scripts/PlaneManager/PlaneMatManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneMatManager : MonoBehaviour
+{
+    [SerializeField]
+    private ARPlaneManager m_ARPlaneManager;
+
+    Material planeMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planeMaterial = m_ARPlaneManager.planePrefab.GetComponent<MeshRenderer>().sharedMaterials[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPress()
+    {
+        m_ARPlaneManager.planePrefab.GetComponent<MeshRenderer>().sharedMaterials[1] = null;
+    }
+
+    public void OnRelease()
+    {
+        m_ARPlaneManager.planePrefab.GetComponent<MeshRenderer>().sharedMaterials[1] = planeMaterial;
+    }
+}
